2a5874b072968e2ad3757d53d01ccdbd6caa6011
hehgithub <1375793491@qq.com>
2019-11-12 20:46:09 +0800

    hjc

--- elec_2/src/main/java/com/wlwaq/elec_2/bean/Identity.java
+++ elec_2/src/main/java/com/wlwaq/elec_2/bean/Identity.java
@@ -10,4 +10,12 @@ public class Identity {
     private String sever_public_twice_dense_step4;
     private String receive_costomer2_dense_step1;
     private String costomer_private_solve_step1;
+    private String device_type;
+    private String device_brand;
+    private String device_version;
+    private String mac;
+    private String ip;
+    private String H_Rb_step1;
+    private String H_Ra_step2;
+    private String status;
 }
